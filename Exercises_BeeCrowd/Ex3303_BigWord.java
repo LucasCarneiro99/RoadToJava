e26f559bb2680b948917a3de2afe942c1b3146ee
LucasCarneiro99 <118139003+LucasCarneiro99@users.noreply.github.com>
2023-03-28 14:51:05 -0300

    Add files via upload

---
+++ Exercises_BeeCrowd/Ex3303_BigWord.java
@@ -0,0 +1,19 @@
+package exercises;
+import java.util.Scanner;
+
+public class Ex3303_BigWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		String x;
+		x = sc.nextLine();
+		if (x.length() >= 10) {
+			System.out.println("palavrao");
+		} else {
+			System.out.println("palavrinha");
+		}
+		sc.close();
+	}
+
+}
