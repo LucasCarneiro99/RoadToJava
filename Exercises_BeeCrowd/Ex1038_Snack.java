72f486fdb6db62430e50724c8878f1654271d299
LucasCarneiro99 <118139003+LucasCarneiro99@users.noreply.github.com>
2023-03-28 19:20:05 -0300

    Add files via upload

---
+++ Exercises_BeeCrowd/Ex1038_Snack.java
@@ -0,0 +1,43 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Ex1038_Snack {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int c,q;
+		double a;
+		c = sc.nextInt();
+		q = sc.nextInt();
+		switch (c) {
+		case 1:
+		a = 4.00*q;
+		System.out.printf("Total: R$ %.2f", a);
+		System.out.println();
+		break;
+		case 2:
+		a = 4.50*q;
+		System.out.printf("Total: R$ %.2f", a);
+		System.out.println();
+		break;
+		case 3:
+		a = 5.00*q;
+		System.out.printf("Total: R$ %.2f", a);
+		System.out.println();
+		break;
+		case 4:
+		a = 2.00*q;
+		System.out.printf("Total: R$ %.2f", a);
+		System.out.println();
+		break;
+		case 5:
+		a = 1.50*q;
+		System.out.printf("Total: R$ %.2f", a);
+		System.out.println();
+		break;
+		}
+		sc.close();
+	}
+
+}
